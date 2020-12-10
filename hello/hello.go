71700206282a0aecdd8300b14ea9e009205522b9
Renny Ren <junhong.ren@longbridge.sg>
2020-12-10 22:30:20 +0800

    return and handle error

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Rbaodf")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Rbaodf")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
