79b519fb7ee2dd28b6f89215a06d2f08c8ac13ae
Renny Ren <junhong.ren@longbridge.sg>
2020-12-13 20:34:30 +0800

    add a test

---
+++ greetings/greetings_test.go
@@ -0,0 +1,22 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Araon"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("Araon")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Araon") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
